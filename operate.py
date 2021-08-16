33a925286fd3458fff7f907a9c7baf26c27fcd9a
Oliver Marks <osmarks@mythic-beasts.com>
2021-08-16 09:56:03 +0100

    neaten code somewhat

--- operate.py
+++ operate.py
@@ -1,3 +1,4 @@
+from logging import currentframe
 import random
 import time
 import requests
@@ -5,7 +6,6 @@ from oauthlib.oauth2 import BackendApplicationClient
 from requests_oauthlib import OAuth2Session
 import paramiko
 import os, os.path
-import base64
 
 if os.path.exists(".env"):
     with open(".env") as f:
@@ -29,31 +29,38 @@ def do_req(method, url, auth, json=None, propagate_http_error=True):
     if propagate_http_error and not (200 <= res.status_code <= 299): raise Exception(f"{method} {url} failed: {res.json().get('error', res.text)} (HTTP {res.status_code})")
     return res
 
+def do_github_req(method, url, *args, **kwargs): return do_req(method, gh_api + url, gh_auth, *args, **kwargs)
+
+current_mb_token = None
+def do_mb_req(method, url, *args, **kwargs):
+    global current_mb_token
+    current_mb_token = acquire_mythic_token(current_mb_token)
+    if not url.startswith(mb_api): url = mb_api + url
+    return do_req(method, url, "Bearer " + current_mb_token["access_token"], *args, **kwargs)
+
 def acquire_mythic_token(current=None):
-    if isinstance(current, dict) and "access_token" in current and current["expires_at"] < (time.time() - 5): return current
+    if isinstance(current, dict) and "access_token" in current and current["expires_at"] > (time.time() + 5): return current
     id, secret = mb_auth
     client = BackendApplicationClient(client_id=id)
     oauth = OAuth2Session(client=client)
     return oauth.fetch_token(client_secret=secret, token_url="https://auth.mythic-beasts.com/login", client_id=id)
 
-def gc_old_servers(token):
+def gc_old_servers():
     # Go through autostarted servers and shut down ones where time string indicates they're more than 12 hours old.
-    for server_id in do_req("GET", f"{mb_api}/beta/vps/servers", f"Bearer {token['access_token']}").json():
+    for server_id in do_mb_req("GET", "/beta/vps/servers").json():
         split = server_id.split("cirunner", 1)
         if len(split) == 2:
             ts = int(split[-1][:10], 16)
             if ts < (time.time() - (60*60*12)):
                 print("Shutting down", server_id)
-                stop_server(token, server_id)
+                stop_server(server_id)
 
 def start_server(server_id):
-    token = acquire_mythic_token()
-    gc_old_servers(token)
+    gc_old_servers()
     if not server_id:
         server_id = f"gha{int(time.time()):09x}{random.randint(0, 0xFFFF_FFFF):08x}"
     key = paramiko.ECDSAKey.generate()
-    result = do_req("POST", f"{mb_api}/beta/vps/servers/{server_id}",
-        f"Bearer {token['access_token']}",
+    result = do_mb_req("POST", f"/beta/vps/servers/{server_id}",
         {
            "product": vdstype,
            "ssh_keys": f"ecdsa-sha2-nistp256 {key.get_base64()}",
@@ -63,16 +70,15 @@ def start_server(server_id):
     )
     # Poll for server boot/install status
     while True:
-        poll_result = do_req("GET", result.headers["Location"], f"Bearer {token['access_token']}").json()
+        poll_result = do_mb_req("GET", result.headers["Location"]).json()
         if poll_result["status"] == "running":
             return key, server_id, poll_result
         else:
             print(poll_result["status"])
-            token = acquire_mythic_token(token)
             time.sleep(10)
 
-def get_runner_by_name(gh_auth, name):
-    runners = do_req("GET", f"{gh_api}/repos/{owner}/{repo}/actions/runners", gh_auth).json()
+def get_runner_by_name(name):
+    runners = do_github_req("GET", f"/repos/{owner}/{repo}/actions/runners").json()
     for runner in runners["runners"]:
         if runner["name"] == name: return runner["id"]
 
@@ -101,16 +107,14 @@ def configure_server(ip, port, key, regtoken, id):
 
 def stop_server(server_id):
     # delete server and runner
-    token = acquire_mythic_token()
-    gc_old_servers(token)
-    do_req("DELETE", f"{mb_api}/beta/vps/servers/{server_id}", f"Bearer {token['access_token']}")
-    rid = get_runner_by_name(gh_auth, server_id)
-    do_req("DELETE", f"{gh_api}/repos/{owner}/{repo}/actions/runners/{rid}", gh_auth)
+    gc_old_servers()
+    do_mb_req("DELETE", f"/beta/vps/servers/{server_id}")
+    rid = get_runner_by_name(server_id)
+    do_github_req("DELETE", f"/repos/{owner}/{repo}/actions/runners/{rid}")
 
 def unsuspend_server(server_id):
-    token = acquire_mythic_token()
     # obtain current status of server to see if it exists or not
-    current_status = do_req("GET", f"{mb_api}/beta/vps/servers/{server_id}", f"Bearer {token['access_token']}", propagate_http_error=False)
+    current_status = do_mb_req("GET", f"/beta/vps/servers/{server_id}", propagate_http_error=False)
     if current_status.status_code == 404 or current_status.json().get("error") == "Server does not exist or access denied":
         if os.environ.get("INPUT_UNSUSPEND-ONLY") == "true":
             raise Exception("Server does not exist and not creating it")
@@ -118,20 +122,19 @@ def unsuspend_server(server_id):
         return start_and_configure(server_id)
     print(current_status.json())
     # if it is down, wake it up and poll until up
-    do_req("PUT", f"{mb_api}/beta/vps/servers/{server_id}/dormant", f"Bearer {token['access_token']}", { "dormant": False, "product": vdstype })
-    do_req("PUT", f"{mb_api}/beta/vps/servers/{server_id}/power", f"Bearer {token['access_token']}", { "power": "power-on" })
+    do_mb_req("PUT", f"/beta/vps/servers/{server_id}/dormant", { "dormant": False, "product": vdstype })
+    do_mb_req("PUT", f"/beta/vps/servers/{server_id}/power", { "power": "power-on" })
     while True:
-        poll_result = do_req("GET", f"{mb_api}/beta/vps/servers/{server_id}", f"Bearer {token['access_token']}").json()
+        poll_result = do_mb_req("GET", f"/beta/vps/servers/{server_id}").json()
         if poll_result["status"] == "running":
             return server_id, poll_result
         else:
             print(poll_result["status"])
-            token = acquire_mythic_token(token)
             time.sleep(5)
 
 def start_and_configure(server_id):
     # generate registration token to initialize a new self-hosted runner
-    runner_token = do_req("POST", f"{gh_api}/repos/{owner}/{repo}/actions/runners/registration-token", auth=gh_auth).json()["token"]
+    runner_token = do_github_req("POST", f"/repos/{owner}/{repo}/actions/runners/registration-token").json()["token"]
     key, id, info = start_server(server_id)
     print(f"::set-output name=server-id::{id}")
     proxy = info["ssh_proxy"]
@@ -142,13 +145,12 @@ def start_and_configure(server_id):
     configure_server(proxy["hostname"], proxy["port"], key, runner_token, id)
 
 def suspend_server(server_id):
-    token = acquire_mythic_token()
     # servers must be powered down before dormancy
-    do_req("PUT", f"{mb_api}/beta/vps/servers/{server_id}/power", f"Bearer {token['access_token']}", { "power": "shutdown" })
+    do_mb_req("PUT", f"/beta/vps/servers/{server_id}/power", { "power": "shutdown" })
     # try for clean shutdown and power off after some time
     time.sleep(10)
-    do_req("PUT", f"{mb_api}/beta/vps/servers/{server_id}/power", f"Bearer {token['access_token']}", { "power": "power-off" })
-    do_req("PUT", f"{mb_api}/beta/vps/servers/{server_id}/dormant", f"Bearer {token['access_token']}", { "dormant": True })
+    do_mb_req("PUT", f"/beta/vps/servers/{server_id}/power", { "power": "power-off" })
+    do_mb_req("PUT", f"/beta/vps/servers/{server_id}/dormant", { "dormant": True })
 
 action = os.environ["INPUT_ACTION"]
 if action == "start":
